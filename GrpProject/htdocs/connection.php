5a5cdb2b4921c5143e77ccf5dd7169f1238b515e
chia9666 <kp.chia9666@gmail.com>
2024-11-01 16:07:48 +0800

    1st acc test

--- GrpProject/htdocs/connection.php
+++ GrpProject/htdocs/connection.php
@@ -1,5 +1,5 @@
 git <?php
-    # change the $db_user, $db_password to your own setup in xampp 2nd acc test
+    # change the $db_user, $db_password to your own setup in xampp 1st acc test
     $serverHost = "localhost";
     $user = "root";
     $password = "root";
