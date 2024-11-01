f8ffa2260417874d62f0ac8e7b84e079a814dd8c
qerelhasiq <khairilhaziqhkr@gmail.com>
2024-11-01 16:44:38 +0800

    Add files via upload

---
+++ ml_application/lib/api_connection/api_connection.dart
@@ -0,0 +1,10 @@
+class API {
+  static const hostConnect = "http://192.168.176.44/server_api";
+
+// registration
+  /*static const validateEmail = "$hostConnect/user/email_validator.php";
+  static const signUp = "$hostConnect/user/signup.php";*/
+
+// login
+  static const login = "$hostConnect/users_api/login.php";
+}
